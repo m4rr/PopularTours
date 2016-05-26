2e1a3267dea530255fc38764ea7601372446cdb6
Marat Saytakov <remarr@gmail.com>
2016-05-26 17:18:12 +0300

    Initial Commit

---
+++ PopularTours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopularTours
+//
+//  Created by Marat S. on 26.05.16.
+//  Copyright © 2016 m4rr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
